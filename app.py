03e8c67a2b67e8da1dd852f0a55993364d01dd1a
Shun Isii <sisii00.swork@gmail.com>
2023-01-07 14:22:54 +0900

    追加

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
+import pathlib
 import aws_cdk as cdk
+from aws_cdk import aws_cloudfront as cloudfront
+from aws_cdk import aws_cloudfront_origins as origins
+from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_s3_deployment as deployment
 
 app = cdk.App()
-stack = cdk.Stack(app, "cdk_cloud_front_s3_oai-stack")
+stack = cdk.Stack(app, "cdk-cloud-front-s3-oai-stack")
 
-# ここに必要なリソース書く
+# バケットを作成する
+bucket = s3.Bucket(stack, "Bucket")
+deployment.BucketDeployment(
+    stack,
+    "BucketDeployment",
+    sources=[deployment.Source.asset("html")],
+    destination_bucket=bucket,
+)
+
+# CloudFrontのディストリビューションを作成する
+cloudfront.Distribution(
+    stack,
+    "Distribution",
+    default_behavior=cloudfront.BehaviorOptions(origin=origins.S3Origin(bucket)),
+    default_root_object="index.html"
+)
 
 app.synth()
